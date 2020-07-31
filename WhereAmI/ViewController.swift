249d98db5c276f763a6094fd95a0392244b9e489
Hamzah Mugharbil <h.mugharbil@gmail.com>
2020-07-30 22:07:46 -0400

    Initial commit.

---
+++ WhereAmI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereAmI
+//
+//  Created by Hamzah Mugharbil on 7/30/20.
+//  Copyright © 2020 Hamzah Mugharbil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
